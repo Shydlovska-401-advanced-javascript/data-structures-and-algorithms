c947d0d206d6fe63d6ce0b392cdd855eda9d02c2
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-08-18 13:43:13 -0700

    findK(), tested findK methos, updated readme

--- data-Structures/linked-list.js
+++ data-Structures/linked-list.js
@@ -1,13 +1,5 @@
 'use strict';
 
-// Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
-// Within your LinkedList class, include a head property. Upon instantiation, an empty Linked List should be created.
-// Define a method called insert which takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance.
-// Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
-// Define a method called toString (or __str__ in Python) which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
-// "{ a } -> { b } -> { c } -> NULL"
-
-
 class Node {
     constructor(val, next =null){
      this.val= val;
@@ -127,9 +119,7 @@ class Node {
     counter--;
     }
     return current.val;
-    }
-    
-    
+    }   
 }
   
   let node = new LinkedList();
