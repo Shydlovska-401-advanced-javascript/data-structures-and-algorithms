53f1905c4b4a0564b153ac6fb10f0600f93904a8
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-09-27 19:13:14 -0700

    added tests + graphs implementation

---
+++ data-Structures/graphs/graphs.js
@@ -0,0 +1,69 @@
+'use strict';
+
+class Edges{
+    constructor(node1, node2, weight=1){
+        this.origin = node1;
+        this.destination = node2;
+        this.weight =weight;
+    }
+}
+
+class Vertex{
+    constructor(val){
+        this.val = val;
+    }
+}
+
+class Graph{
+    constructor(){
+        this.vertices =[];
+        this.edges = {};
+        this.numberOfEdges =0;
+
+    }
+
+    addVertex(value){
+        let vertex = new Vertex(value);
+        this.vertices.push(vertex);
+        this.edges[vertex]= [];
+        return vertex;
+    }
+
+    addEdge(origin, destination, weight=1){
+        if(!this.edges[origin] || !this.edges[destination]) {
+            return null;
+        }
+        let edge = new Edges(origin, destination, weight);
+        this.edges[origin].push(edge);
+        this.numberOfEdges++;
+    }
+
+    getNeigbors(vertex){
+        return Object.values(this.edges).flat().filter(edge => edge.origin === vertex || edge.destination === vertex);
+    }
+
+    getVertex(){
+      return this.vertices;
+    }
+
+    size(){
+      return this.vertices.length;
+    }
+
+}
+
+module.exports = Graph;
+
+
+// let graph = new Graph()
+
+// let nodeKate = graph.addVertex('Kate')
+// let nodeHulk= graph.addVertex('Hulk')
+// let nodeMac = graph.addVertex('Mac')
+
+// graph.addEdge(nodeKate, nodeHulk)
+
+// graph.addEdge(nodeMac, nodeHulk)
+// graph.addEdge(nodeKate, nodeMac)
+
+// console.log(graph.getNeigbors(nodeKate))
--- data-Structures/graphs/graphs.test.js
+++ data-Structures/graphs/graphs.test.js
@@ -1,28 +1,62 @@
 'use strict';
 
-class Graph{
-    constructor(){
+const Graph = require('./graphs.js')
 
-    }
+let graph = new Graph();
 
-    addNode(){
 
-    }
+   it('Node can be successfully added to the graph', () => {
 
-    addEdge(){
 
-    }
+    expect(graph.addVertex('Kate')).toEqual({val : "Kate"})
+    expect(graph.addVertex('Hulk')).toEqual({val : "Hulk"})
+});
 
-    getNeigbors(){
 
-    }
 
-    getNodes(){
+it('An edge can be successfully added to the graph ', () => {
+    let newGraph = new Graph();
+    let nodeKate = newGraph.addVertex('Kate');
+    let nodeHulk = newGraph.addVertex('Hulk');
+    newGraph.addEdge(nodeKate, nodeHulk);
+    let neighbors = newGraph.getNeigbors(nodeKate)
+    expect(neighbors.length).toBe(1)
+});
 
-    }
 
-    size(){
-        
-    }
+it('A collection of all nodes can be properly retrieved from the graph', () => {
+   let nodes = graph.getVertex()
+    expect(nodes.length).toBe(2)
+    expect(nodes[0].val).toBe("Kate")
 
-}
+});
+
+it('All appropriate neighbors can be retrieved from the graph ', () => {
+    let newGraph = new Graph();
+    let nodeKate = newGraph.addVertex('Kate');
+    let nodeHulk = newGraph.addVertex('Hulk');
+    newGraph.addEdge(nodeKate, nodeHulk);
+    let neighbors = newGraph.getNeigbors(nodeKate)
+    expect(neighbors[0].origin).toEqual({val: "Kate"})
+    expect(neighbors[0].destination).toEqual({val: "Hulk"})
+});
+
+it(' Neighbors are returned with the weight between nodes included ', () => {
+    let newGraph = new Graph();
+    let nodeKate = newGraph.addVertex('Kate');
+    let nodeHulk = newGraph.addVertex('Hulk');
+    newGraph.addEdge(nodeKate, nodeHulk, 100);
+    let neighbors = newGraph.getNeigbors(nodeKate)
+    expect(neighbors[0].origin).toEqual({val: "Kate"})
+    expect(neighbors[0].destination).toEqual({val: "Hulk"})
+
+    expect(neighbors[0].weight).toBe(100)
+});
+
+
+it(' The proper size is returned, representing the number of nodes in the graph', () => {
+
+    expect(graph.size()).toBe(2)
+});
+
+
